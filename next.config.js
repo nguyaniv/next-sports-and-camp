e046e2ccf0462aa67915c41551d3688abf036a61
unknown <nguyaniv@gmail.com>
2022-01-08 00:06:44 +0200

    add progress bar before switching pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  "noImplicitAny": false,
   images: {
     domains: [
       's.gravatar.com',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,40 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../app/store';
-import { UserProvider } from '@auth0/nextjs-auth0';
-
-import Layout from '../cmps/Layout/Layout';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import NProgress from "nprogress";
+import Router from "next/router";
+import Layout from "../cmps/Layout/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.configure({ showSpinner: false });
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
   return (
-    <UserProvider>
-      <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
-    </UserProvider>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <UserProvider>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
+      </UserProvider>
+    </>
   );
 }
 
